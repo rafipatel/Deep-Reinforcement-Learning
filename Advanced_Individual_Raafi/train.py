27f58e6d7d0970ebd4d44f7252942f0963f9e402
Rafi Patel <rafipatel3562@gmail.com>
2024-05-11 04:15:08 +0100

    minor

--- Advanced_Individual_Raafi/train.py
+++ Advanced_Individual_Raafi/train.py
@@ -1,7 +1,5 @@
 import ray.rllib.algorithms.dqn as dqn
 import ray
-from ray import tune
-
 
 env_name = "Breakout-v4"
 
@@ -10,14 +8,14 @@ config = dqn.DQNConfig()
 config["gamma"] = 0.99
 # config["gamma"] = tune.grid_search([0.9, 0.99])
 config["train_batch_size"] = 64
-config["timesteps_per_iteration"] = 2000
+config["timesteps_per_iteration"] = 1000
 config["target_network_update_freq"] = 600
-config["num_gpus"] = 0
+config["num_gpus"] = 1
 config["model"]["fcnet_hiddens"] = [128, 128, 128]
 config["model"]["fcnet_activation"] = "relu"
 config["env"] = "BreakoutDeterministic-v4"
 config["framework"] = "torch"
-config["double_q"] = True
+# config["double_q"] = True
 config["n_step"] = 3
 config["exploration_fraction"] = 0.1
 config["exploration_final_eps"] = 0.02
@@ -29,19 +27,7 @@ config["lr"] = 0.0005
 
 if __name__ == '__main__':
 
-    print("run started")
-
-    ray.tune.run("DQN", name="Breakout_Model", stop={"timesteps_total": 1000000}, checkpoint_freq=20, config=config,
-                         local_dir = "/users/adfx757/ray_results/" + env_name,
+    ray.tune.run("DQN", name="Breakout_Model", stop={"timesteps_total": 100000}, checkpoint_freq=200, config=config,
+                         local_dir = "/content/ray_results1221/" + env_name,
                         )
 
-
-
-# tune.run(
-#     "PPO",
-#     name = f"PPO_{env_name}_multi-agent_normal",
-#     stop = {"timesteps_total": 5000000},
-#     checkpoint_freq = 100,
-#     local_dir = "~/ray_results/" + env_name,
-#     config = config,
-# )
